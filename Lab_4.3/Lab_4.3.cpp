5c5801a5924d62e5cb6d5350517ba771571178b1
zasidkayuliia <yuliia.zasidka.ri.2024@lpnu.ua>
2024-09-27 16:34:01 +0300

    first commit

---
+++ Lab_4.3/Lab_4.3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, F, a, b,c;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << fixed;
+	cout << "----------------------" << endl;
+	cout << "|" << setw(5) << "x" << "   |"
+		<< setw(7) << "F" << "    |" << endl;
+	cout << "----------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x < 3 && b != 0)
+			F = a * x * x - b * x + c;
+		else
+			if (x > 3 && b == 0)
+				F = (x - a) / (x - c);
+			else
+				F = x / c;
+
+
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << F
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "----------------------" << endl;
+	return 0;
+}
